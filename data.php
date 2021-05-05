2b6d54006e43a14fff6cae249d942254a5384d18
Swati-12345678 <swatikathrotiya1019@gmail.com>
2021-05-05 09:04:53 -0700

    initial

--- data.php
+++ data.php
@@ -1,16 +1,18 @@
-<?php
+<!DOCTYPE html>
+<head>
+	<title>  Hospital System </title>
+	
+	<link href="css/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<h1>Data Saved Successfully...</h1><Br>
 
-$email=$_GET["emailid"];
-$pass=$_GET["pass1"];
-
-if(!empty($email) && !empty($pass))
-{
-    echo "<b>Email_id :: </b>".$email."<br>";
-    echo "<b>Password :: </b>".$pass."<br>";
-}
-else
-{
-    echo "<b>Error occur...  </b>Fill all the feilds..!";
-}
-
-?>
+<b>FirstName :- </b> <?php echo $_POST["firstname"]; ?><br>
+<b>LastName :- </b> <?php echo $_POST["lastname"]; ?><br>
+<b>Email :- </b> <?php echo $_POST["email"]; ?><br>
+<b>Gender :- </b> <?php echo $_POST["gender"]; ?><br>
+<b>Mobile No :- </b> <?php echo $_POST["mobile"]; ?><br>
+<b>Date :- </b> <?php echo $_POST["date"]; ?><br>
+<b>Topic :- </b> <?php echo $_POST["msg"]; ?><br>
+</body>
+</html>
