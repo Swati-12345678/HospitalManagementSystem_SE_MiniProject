611a539723cc4c27ca9158fad2c20bfdabb2fb01
Swati-12345678 <swatikathrotiya1019@gmail.com>
2021-05-05 21:15:29 -0700

    initial

--- data.php
+++ data.php
@@ -3,8 +3,29 @@
 	<title>  Hospital System </title>
 	
 	<link href="css/style.css" type="text/css" rel="stylesheet">
+	<style>
+a:link, a:visited {
+  background-color: black;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+</style>
 </head>
 <body>
+
+<div class="top">
+		<div>
+		 Contact Us +91 87564 20000 / 01 | abc@hospital.com
+		</div>
+	</div>
+	
+	
+	
+
 <h1>Data Saved Successfully...</h1><Br>
 
 <b>FirstName :- </b> <?php echo $_POST["firstname"]; ?><br>
@@ -13,6 +34,9 @@
 <b>Gender :- </b> <?php echo $_POST["gender"]; ?><br>
 <b>Mobile No :- </b> <?php echo $_POST["mobile"]; ?><br>
 <b>Date :- </b> <?php echo $_POST["date"]; ?><br>
-<b>Topic :- </b> <?php echo $_POST["msg"]; ?><br>
+<b>Topic :- </b> <?php echo $_POST["msg"]; ?><br><br><br>
+
+<a href="index.html">Back To Home</a>
+
 </body>
 </html>
